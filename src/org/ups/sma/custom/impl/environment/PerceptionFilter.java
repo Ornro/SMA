594e13867811862ee588ea8af89701d794a31e12
Axel Robert <robert.axel31@gmail.com>
2014-06-09 19:40:14 +0200

    implementation of Perception and Range Filters

--- src/org/ups/sma/custom/impl/environment/PerceptionFilter.java
+++ src/org/ups/sma/custom/impl/environment/PerceptionFilter.java
@@ -4,6 +4,8 @@ import org.ups.sma.custom.domain.environment.Location;
 import org.ups.sma.domain.environnement.Filter;
 import org.ups.sma.impl.agent.Agent;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  * Created by Business on 09/06/14.
  */
@@ -17,11 +19,23 @@ public class PerceptionFilter extends Filter {
     public boolean isAcceptable(Location location, Agent agent) {
         Location currentLocation = agent.getLocation();
         for(int i=0; i<3; i++) {
-            Location loc = new Location(currentLocation.x, currentLocation.y + i);
+            Location locPlusY = new Location(currentLocation.x, currentLocation.y + i);
+            Location locMinusY = new Location(currentLocation.x, currentLocation.y - i);
+            Location locPlusX = new Location(currentLocation.x + 1, currentLocation.y);
+            Location locMinusX = new Location(currentLocation.x - 1, currentLocation.y + i);
+            Location locPlusXY = new Location(currentLocation.x + 1, currentLocation.y + i);
+            Location locPlusXMinusY = new Location(currentLocation.x + 1, currentLocation.y - i);
+            Location locMinusXPlusY = new Location(currentLocation.x - 1, currentLocation.y + i);
+            Location locMinusXY = new Location(currentLocation.x - 1, currentLocation.y - i);
 
-            if(loc.equals(currentLocation))
+            if(locPlusY.equals(currentLocation) || locMinusY.equals(currentLocation)
+                    || locPlusX.equals(currentLocation) || locMinusX.equals(currentLocation)
+                    || locPlusXY.equals(currentLocation) || locPlusXMinusY.equals(currentLocation)
+                    || locMinusXPlusY.equals(currentLocation) || locMinusXY.equals(currentLocation)) {
                 return true;
+            }
         }
 
+        return false;
     }
 }
--- src/org/ups/sma/custom/impl/environment/RangeFilter.java
+++ src/org/ups/sma/custom/impl/environment/RangeFilter.java
@@ -13,6 +13,23 @@ public class RangeFilter extends Filter {
 
     @Override
     public boolean isAcceptable(Location location, Agent agent) {
-        return false;
+        Location currentLocation = agent.getLocation();
+        Location locPlusY = new Location(currentLocation.x, currentLocation.y + 1);
+        Location locMinusY = new Location(currentLocation.x, currentLocation.y - 1);
+        Location locPlusX = new Location(currentLocation.x + 1, currentLocation.y);
+        Location locMinusX = new Location(currentLocation.x - 1, currentLocation.y + 1);
+        Location locPlusXY = new Location(currentLocation.x + 1, currentLocation.y + 1);
+        Location locPlusXMinusY = new Location(currentLocation.x + 1, currentLocation.y - 1);
+        Location locMinusXPlusY = new Location(currentLocation.x - 1, currentLocation.y + 1);
+        Location locMinusXY = new Location(currentLocation.x - 1, currentLocation.y - 1);
+
+        if(locPlusY.equals(currentLocation) || locMinusY.equals(currentLocation)
+                || locPlusX.equals(currentLocation) || locMinusX.equals(currentLocation)
+                || locPlusXY.equals(currentLocation) || locPlusXMinusY.equals(currentLocation)
+                || locMinusXPlusY.equals(currentLocation) || locMinusXY.equals(currentLocation)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
